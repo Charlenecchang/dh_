5c7529d65ddc30311dfdf0c2f297d7744596f959
Charlotte Liu <lsh355@uw.edu>
2024-10-12 16:49:51 -0700

    folder added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/student-orgs">Student Orgs</Link></li>
+          <li><Link to="/career-fair">Career Fair</Link></li>
+          <li><Link to="/internships">Internships</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome back, Charlotte!</h1>
+      <p>Here are the latest updates:</p>
+    </div>
+  );
+};
+
+export default Home;
