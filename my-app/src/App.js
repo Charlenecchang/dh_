530a8af84d7ffe594309b8a8462aeb644faa1ed6
charlene <charleneliesl@gmail.com>
2024-10-12 19:25:29 -0700

    Study Match Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './styles.css'; // Import the CSS globally
+import StudyMatch from './pages/StudyMatch';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/studymatch" element={<StudyMatch />} />
       </Routes>
     </Router>
   );
